5ffe87684b58410a9cf2fc52e17bb2d137e0988e
Velpula,SravanKumar <sravan_velpula@optum.com>
2023-12-04 16:03:53 +0530

    Complete JS Map Filter Reduce Find Arrow functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,21 +3,29 @@ import Header from "./Header";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
-function createEntry(emojielement) {
-    return <Entry 
-            key={emojielement.id}
-            emoji={emojielement.emoji}
-            name={emojielement.name}
-            meaning={emojielement.meaning}
-        />;
-}
+// function createEntry(emojielement) {
+//     return <Entry 
+//             key={emojielement.id}
+//             emoji={emojielement.emoji}
+//             name={emojielement.name}
+//             meaning={emojielement.meaning}
+//         />;
+// }
 
 function App() {
   return (
     <div>
       <Header/>
       <dl className="dictionary">
-        {emojipedia.map(createEntry)};        
+        {/* emojipedia.map(createEntry) */}
+        {emojipedia.map(emojielement => 
+        <Entry 
+            key={emojielement.id}
+            emoji={emojielement.emoji}
+            name={emojielement.name}
+            meaning={emojielement.meaning}
+        />
+          )};        
       </dl>
     </div>
   );
